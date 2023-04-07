8b534805ef79a13fe6a9b08b6d340c951573d0f7
Tony-Hany <119168690+Tony-Hany@users.noreply.github.com>
2023-04-07 21:27:45 +0200

    INIT COMPLET + test working blinking body led

---
+++ src/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <main.h>
+
+#include "leds.h"
+
+
+int main(void)
+{
+
+    halInit();
+    chSysInit();
+    mpu_init();
+
+
+    /* Infinite loop. */
+    while (1) {
+    	//waits 1 second
+        set_body_led(1);
+        chThdSleepMilliseconds(1000);
+        set_body_led(0);
+        chThdSleepMilliseconds(1000);
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
