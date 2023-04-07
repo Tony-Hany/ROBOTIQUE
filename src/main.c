7916d0007c6a036a45b4378814e15050d94f84e2
Tony-Hany <119168690+Tony-Hany@users.noreply.github.com>
2023-04-07 22:20:43 +0200

    cooperative scheduling testing started

--- src/main.c
+++ src/main.c
@@ -10,23 +10,46 @@
 
 #include "leds.h"
 
+static THD_WORKING_AREA(waThread1, 128);
+static THD_FUNCTION(Thread1, arg) {
+  (void)arg;
+  chRegSetThreadName("Thread1");
+  while (true) {
+    //Operation block requires (100ms)
+    chThdYield();
+  }
+}
+static THD_WORKING_AREA(waThread2, 128);
+static THD_FUNCTION(Thread2, arg) {
+  (void)arg;
+  chRegSetThreadName("Thread2");
+  while (true) {
+    //Operation block requires (50ms)
+    chThdYield();
+  }
+}
+static THD_WORKING_AREA(waThread3, 128);
+static THD_FUNCTION(Thread3, arg) {
+  (void)arg;
+  chRegSetThreadName("Thread3");
+  while (true) {
+    //Operation block requires (150ms)
+    chThdYield();
+  }
+}
+int main(void) {
+  halInit();
+  chSysInit();
+  mpu_init();
 
-int main(void)
-{
-
-    halInit();
-    chSysInit();
-    mpu_init();
-
-
-    /* Infinite loop. */
-    while (1) {
-    	//waits 1 second
-        set_body_led(1);
-        chThdSleepMilliseconds(1000);
-        set_body_led(0);
-        chThdSleepMilliseconds(1000);
-    }
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+  chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, Thread2, NULL);
+  chThdCreateStatic(waThread3, sizeof(waThread3), NORMALPRIO, Thread3, NULL);
+ 
+  while (true) {
+    //Operation block requires (200ms)
+    chThdYield();
+  }
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
